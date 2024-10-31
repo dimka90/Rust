b5ff6c28c8e6a9c85027784328bd5156b70a6252
dimka90 <dimkayilrit@gmail.com>
2024-10-31 08:48:52 +0100

    write the

---
+++ hello_cargo/target/debug/deps/hello_cargo-70b7650f2196efb1.d
@@ -0,0 +1,5 @@
+/home/dimka/Desktop/BlockchainProjects/Rust/hello_cargo/target/debug/deps/hello_cargo-70b7650f2196efb1: src/main.rs
+
+/home/dimka/Desktop/BlockchainProjects/Rust/hello_cargo/target/debug/deps/hello_cargo-70b7650f2196efb1.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/dimka/Desktop/BlockchainProjects/Rust/hello_cargo/target/debug/hello_cargo: /home/dimka/Desktop/BlockchainProjects/Rust/hello_cargo/src/main.rs
